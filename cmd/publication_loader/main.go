4ac1f128f08112a1a032f01749344c9f93a75cfa
makaires77 <marcos@bahlis.com.br>
2023-05-20 10:02:04 -0300

    ajuste nome

--- cmd/publication_loader/main.go
+++ cmd/publication_loader/main.go
@@ -2,49 +2,64 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/makaires77/ppgcs/pkg/infrastructure/dgraph"
-	"github.com/makaires77/ppgcs/pkg/infrastructure/json"
-	"github.com/makaires77/ppgcs/pkg/interfaces/http/handlers"
-	"github.com/makaires77/ppgcs/pkg/usecase/load_publication"
+	json "github.com/makaires77/ppgcs/pkg/infrastructure/json_publication"
 )
 
 func main() {
-	// Load JSON data
-	publicationData, err := json.LoadPublicationData("_data/in_json/642.files/642.publication.json")
+	// Defina os caminhos para os arquivos JSON de publicações
+	jsonFilePath1 := "_data/in_json/642.files/642.publication.json"
+	jsonFilePath2 := "_data/in_json/642.files/644.publication.json"
+
+	// Carregue as entidades dos arquivos JSON
+	periodicos1, err := json.LoadEntitiesFromJSON(jsonFilePath1)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Erro ao carregar as entidades do arquivo %s: %v\n", jsonFilePath1, err)
+		os.Exit(1)
 	}
 
-	// Create Dgraph client
-	client, err := dgraph.NewDgraphClient()
+	periodicos2, err := json.LoadEntitiesFromJSON(jsonFilePath2)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-
-	// Create repository
-	repo := dgraph.NewPublicationRepository(client)
-
-	// Create use case interactor
-	interactor := load_publication.NewInteractor(repo)
-
-	// Load publications
-	err = interactor.LoadPublications(publicationData)
-	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Erro ao carregar as entidades do arquivo %s: %v\n", jsonFilePath2, err)
+		os.Exit(1)
 	}
 
-	// Start API server
-	handler := handlers.NewPublicationHandler(interactor)
-	addr := ":8080"
-	fmt.Printf("Starting API server at %s\n", addr)
-	err = handler.StartServer(addr)
-	if err != nil {
-		log.Fatal(err)
+	// Criar um mapa para rastrear as publicações existentes pelo campo Hash
+	publicacoesExist := make(map[string]bool)
+
+	// Percorra as publicações do primeiro arquivo JSON e adicione ao mapa de publicações existentes
+	for _, anos := range periodicos1 {
+		for _, publicacoes := range anos {
+			for _, publicacao := range publicacoes {
+				publicacoesExist[publicacao.Hash] = true
+			}
+		}
 	}
 
-	os.Exit(0)
+	// Percorra as publicações do segundo arquivo JSON e adicione ao mapa de publicações existentes, evitando duplicações
+	for _, anos := range periodicos2 {
+		for _, publicacoes := range anos {
+			for _, publicacao := range publicacoes {
+				if !publicacoesExist[publicacao.Hash] {
+					publicacoesExist[publicacao.Hash] = true
+					// Adicione a publicação aos períodicos existentes
+					periodicos1[publicacao.Periodico][publicacao.Ano] = append(periodicos1[publicacao.Periodico][publicacao.Ano], publicacao)
+				}
+			}
+		}
+	}
+
+	// Imprima as entidades carregadas, incluindo as novas do segundo arquivo, para verificar se está tudo correto
+	for periodico, anos := range periodicos1 {
+		fmt.Printf("Periodico: %s\n", periodico)
+		for ano, publicacoes := range anos {
+			fmt.Printf("  Ano: %s\n", ano)
+			for _, publicacao := range publicacoes {
+				fmt.Printf("    Titulo: %s\n", publicacao.Titulo)
+				fmt.Printf("    Autores: %v\n", publicacao.Autores)
+				// Imprima outras propriedades conforme necessário...
+			}
+		}
+	}
 }
