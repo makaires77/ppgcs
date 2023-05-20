6999b8b4acfc2028ebe148cdafabbaaf29afb912
makaires77 <marcos@bahlis.com.br>
2023-05-20 09:45:21 -0300

    main.go correto

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/makaires77/ppgcs/pkg/domain/publication/repository"
+	"github.com/makaires77/ppgcs/pkg/domain/publication"
 	"github.com/makaires77/ppgcs/pkg/interfaces/http/handlers"
 	"github.com/makaires77/ppgcs/pkg/usecase/load_publication"
 )
 
 func main() {
-	// Configuração do repositório de publicação
-	publicationRepo := repository.NewPublicationRepository()
+	// Crie uma instância do repositório do Publication
+	publicationRepository := publication.NewInMemoryPublicationRepository()
 
-	// Configuração do interactor de publicação
-	publicationInteractor := load_publication.NewPublicationInteractor(publicationRepo)
+	// Crie uma instância do interactor do Publication
+	publicationInteractor := load_publication.NewPublicationInteractor(publicationRepository)
 
-	// Configuração do handler de publicação
+	// Crie uma instância do handler do Publication
 	publicationHandler := handlers.NewPublicationHandler(publicationInteractor)
 
-	// Configuração do roteador
+	// Crie um roteador usando o Gorilla Mux
 	router := mux.NewRouter()
-	router.HandleFunc("/publications", publicationHandler.GetAllPublications).Methods("GET")
 
-	// Inicialização do servidor HTTP
-	log.Println("Servidor iniciado na porta 8080")
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
-		log.Fatalf("Falha ao iniciar o servidor HTTP: %v", err)
-	}
+	// Configure as rotas do Publication
+	router.HandleFunc("/publications", publicationHandler.GetPublications).Methods("GET")
+
+	// Inicie o servidor HTTP
+	fmt.Println("API do PPGCS em execução na porta 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
