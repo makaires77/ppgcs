cbde25714993af9f4b38ae75ea8540765165f251
makaires77 <marcos@bahlis.com.br>
2023-05-22 10:28:40 -0300

    remodelagem

--- pkg/infrastructure/scrap_lattes/scrap_lattes.go
+++ pkg/infrastructure/scrap_lattes/scrap_lattes.go
@@ -1,440 +1,94 @@
 package scrap_lattes
 
 import (
+	"encoding/csv"
+	/* 	"errors"
+	 */
 	"fmt"
-	"time"
-
-	"github.com/pkg/errors"
+	"io"
+	"os"
+	"sync"
 )
 
 type ScrapLattes struct {
 	// Adicione aqui os campos necessários para a conexão com bancos de dados, como clientes MongoDB, Neo4j, etc.
 }
 
-type Pesquisador struct {
-	Nome                       string
-	Titulo                     string
-	LinkCurriculo              string
-	IDLattes                   string
-	DataUltimaAtualizacao      string
-	Resumo                     string
-	NomeCitacoesBibliograficas string
-	IDLattesLink               string
-	OrcidID                    string
-	Formacao                   []string
-	Formacoes                  []FormacaoAcademica
-	PosDoutorado               []FormacaoPosDoc
-	FormacoesComplementares    []FormacaoComplementar
-	AtuacoesProfissionais      []AtuacaoProfissional
-	LinhasPesquisa             []LinhaPesquisa
-	ProjetosPesquisa           []ProjetoPesquisa       `json:"projetos_pesquisa"`
-	Publicacoes                []Publicacao            `json:"publicacoes"`
-	FormacaoAcademica          []Formacao              `json:"formacao_academica"`
-	CursosExtraCurriculares    []Curso                 `json:"cursos_extra_curriculares"`
-	ExperienciaProfissional    []Experiencia           `json:"experiencia_profissional"`
-	Patentes                   []Patente               `json:"patentes"`
-	MembroCorpoEditorial       []MembroCorpoEditorial  `json:"membro_corpo_editorial"`
-	RevisorPeriodico           []RevisorPeriodico      `json:"revisor_periodico"`
-	RevisorProjetoFomento      []RevisorProjetoFomento `json:"revisor_projeto_fomento"`
-	PremiosTitulos             []PremioTitulo          `json:"premios_titulos"`
-}
+// Funcionalidades:
 
-type FormacaoAcademica struct {
-	Periodo        string
-	Nivel          string
-	TituloTrabalho string
-	Instituicao    string
-	Orientador     string
-	Bolsista       string
-	AreaEstudo     string
-	PalavrasChave  []string
-}
-
-type FormacaoPosDoc struct {
-	Periodo        string
-	Nivel          string
-	TituloTrabalho string
-	Instituicao    string
-	Orientador     string
-	Bolsista       string
-	AreaEstudo     string
-	PalavrasChave  []string
-}
-
-type FormacaoComplementar struct {
-	Periodo      string
-	TituloCurso  string
-	CargaHoraria string
-	Instituicao  string
-}
-
-type AtuacaoProfissional struct {
-	Instituicao          string
-	VinculoInstitucional []VinculoInstitucional
-	Atividades           []Atividade
-}
-
-type VinculoInstitucional struct {
-	Periodo      string
-	Descricao    string
-	CargaHoraria int
-}
-
-type Atividade struct {
-	Periodo        string
-	Descricao      string
-	Disciplinas    []string
-	LinhasPesquisa []string
-}
-
-type LinhaPesquisa struct {
-	Nome             string
-	Objetivo         string
-	GrandeArea       []string
-	Area             []string
-	SubArea          []string
-	SetoresAtividade []string
-	PalavrasChave    []string
-}
-
-type Endereco struct {
-	Rua    string `json:"rua"`
-	Bairro string `json:"bairro"`
-	Cidade string `json:"cidade"`
-	Estado string `json:"estado"`
-	Pais   string `json:"pais"`
-	CEP    string `json:"cep"`
-}
-
-type RedeSocial struct {
-	Nome string `json:"nome"`
-	URL  string `json:"url"`
-}
-
-type ProjetoPesquisa struct {
-	Titulo        string     `json:"titulo"`
-	Descricao     string     `json:"descricao"`
-	DataInicio    *time.Time `json:"data_inicio"`
-	DataTermino   *time.Time `json:"data_termino"`
-	Financiamento string     `json:"financiamento"`
-}
-
-type Publicacao struct {
-	Titulo  string     `json:"titulo"`
-	Resumo  string     `json:"resumo"`
-	Data    *time.Time `json:"data"`
-	Revista string     `json:"revista"`
-	Ano     string
-	Autores []string
-	DOI     string
-}
-
-type Formacao struct {
-	Titulo       string `json:"titulo"`
-	Instituicao  string `json:"instituicao"`
-	AnoConclusao string `json:"ano_conclusao"`
-}
-
-type Curso struct {
-	Nome          string     `json:"nome"`
-	Instituicao   string     `json:"instituicao"`
-	DataConclusao *time.Time `json:"data_conclusao"`
-}
-
-type Experiencia struct {
-	Posicao string     `json:"posicao"`
-	Empresa string     `json:"empresa"`
-	Inicio  *time.Time `json:"inicio"`
-	Termino *time.Time `json:"termino"`
-}
-
-type Patente struct {
-	Titulo string `json:"titulo"`
-	Resumo string `json:"resumo"`
-	Ano    int    `json:"ano"`
-}
-
-type MembroCorpoEditorial struct {
-	Periodo   Periodo `json:"periodo"`
-	Periodico string  `json:"periodico"`
-}
-
-type Periodo struct {
-	Inicio  *time.Time `json:"inicio"`
-	Termino *time.Time `json:"termino"`
-}
-
-type RevisorPeriodico struct {
-	Periodo   Periodo `json:"periodo"`
-	Periodico string  `json:"periodico"`
-}
-
-type RevisorProjetoFomento struct {
-	Periodo        Periodo `json:"periodo"`
-	AgenciaFomento string  `json:"agencia_fomento"`
-}
-
-type PremioTitulo struct {
-	Ano       int    `json:"ano"`
-	Descricao string `json:"descricao"`
-}
-
-func NewScrapLattes() *ScrapLattes {
-	// Inicialize e retorne uma instância de ScrapLattes
-	return &ScrapLattes{}
-}
-
-func (s *ScrapLattes) ObterPesquisador(IDLattes string) (*Pesquisador, error) {
-	// Lógica para obter um pesquisador pelo IDLattes
-
-	// Verifique se o IDLattes é válido
-	if IDLattes == "" {
-		return nil, errors.New("IDLattes inválido")
-	}
-
-	// Consulte o banco de dados para obter o pesquisador com o IDLattes especificado
-	pesquisador, err := s.ObterPesquisadorPorIDLattes(IDLattes)
+func (s *ScrapLattes) ProcessarArquivo(filePath string) error {
+	// Abrir o arquivo CSV
+	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, errors.Wrap(err, "falha ao obter pesquisador do banco de dados")
+		return err
 	}
+	defer file.Close()
 
-	// Retorne o pesquisador encontrado ou um erro, se aplicável
-	if pesquisador == nil {
-		return nil, errors.New("pesquisador não encontrado")
-	}
+	// Criar um novo leitor CSV
+	reader := csv.NewReader(file)
 
-	return pesquisador, nil
-}
+	// Iniciar um novo grupo de goroutines
+	var wg sync.WaitGroup
 
-func (s *ScrapLattes) SalvarPesquisador(pesquisador *Pesquisador) error {
-	// Lógica para salvar um pesquisador em algum repositório
+	// Criar um canal para enviar registros de linha do CSV
+	c := make(chan []string)
 
-	// Verifique se o pesquisador é nulo
-	if pesquisador == nil {
-		return errors.New("pesquisador inválido")
-	}
+	// Iniciar a goroutine para ler o arquivo e enviar linhas no canal
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 
-	// Verifique se o pesquisador possui todas as informações obrigatórias
-	if pesquisador.Nome == "" {
-		return errors.New("o nome do pesquisador é obrigatório")
-	}
+		for {
+			record, err := reader.Read()
 
-	if pesquisador.Titulo == "" {
-		return errors.New("o título do pesquisador é obrigatório")
-	}
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
 
-	// Salve o pesquisador no banco de dados
-	err := s.SalvarPesquisadorNoBancoDeDados(pesquisador)
-	if err != nil {
-		return errors.Wrap(err, "falha ao salvar pesquisador no banco de dados")
-	}
+				fmt.Println("Erro ao ler a linha:", err)
+			}
 
-	// Exemplo de impressão de mensagem de sucesso
-	fmt.Println("Pesquisador salvo com sucesso!")
+			c <- record
+		}
+
+		// Fechar o canal após todas as linhas serem lidas
+		close(c)
+	}()
+
+	// Iniciar a goroutine para processar registros enviados no canal
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for record := range c {
+			err := s.ProcessarRegistro(record)
+
+			if err != nil {
+				fmt.Println("Erro ao processar registro:", err)
+			}
+		}
+	}()
+
+	// Esperar ambas goroutines finalizarem
+	wg.Wait()
 
 	return nil
 }
 
-func (s *ScrapLattes) AtualizarPesquisador(pesquisador *Pesquisador) error {
-	// Lógica para atualizar um pesquisador em algum repositório
+func (s *ScrapLattes) ProcessarRegistro(record []string) error {
+	// Extrair informações do registro
 
-	// Verifique se o pesquisador é nulo
-	if pesquisador == nil {
-		return errors.New("pesquisador inválido")
-	}
+	nome := record[0]
+	lattes := record[1]
+	// ...
 
-	// Verifique se o pesquisador existe no repositório
-	existe, err := s.PesquisadorExiste(pesquisador.IDLattes)
-	if err != nil {
-		return errors.Wrap(err, "falha ao verificar a existência do pesquisador")
-	}
+	// Exemplo de operação: exibir informações
+	fmt.Println("Nome:", nome)
+	fmt.Println("Lattes:", lattes)
+	fmt.Println("---")
 
-	if !existe {
-		return errors.New("pesquisador não encontrado")
-	}
-
-	// Atualize os campos do pesquisador no repositório
-	err = s.AtualizarPesquisadorNoRepositorio(pesquisador)
-	if err != nil {
-		return errors.Wrap(err, "falha ao atualizar pesquisador no repositório")
-	}
-
-	// Exemplo de impressão de mensagem de sucesso
-	fmt.Println("Pesquisador atualizado com sucesso!")
+	// Adicione aqui qualquer lógica adicional para processar a linha
 
 	return nil
-}
-
-func (s *ScrapLattes) PesquisadorExiste(IDLattes string) (bool, error) {
-	// Lógica para verificar se um pesquisador existe no repositório
-
-	// Verifique se o IDLattes é válido
-	if IDLattes == "" {
-		return false, errors.New("IDLattes inválido")
-	}
-
-	// Verifique no banco de dados se o pesquisador com o IDLattes especificado existe
-	existe, err := s.VerificarExistenciaPesquisadorNoBancoDeDados(IDLattes)
-	if err != nil {
-		return false, errors.Wrap(err, "falha ao verificar existência do pesquisador no banco de dados")
-	}
-
-	return existe, nil
-}
-
-func (s *ScrapLattes) ObterPublicacoes(pesquisador *Pesquisador) ([]Publicacao, error) {
-	// Lógica para obter as publicações de um pesquisador
-
-	// Verifique se o pesquisador é nulo
-	if pesquisador == nil {
-		return nil, errors.New("pesquisador inválido")
-	}
-
-	// Obtenha as publicações do pesquisador
-	publicacoes, err := s.ObterPublicacoesDoPesquisador(pesquisador)
-	if err != nil {
-		return nil, errors.Wrap(err, "falha ao obter publicações do pesquisador")
-	}
-
-	return publicacoes, nil
-}
-
-func (s *ScrapLattes) ImportarDadosCSV(filePath string) error {
-	// Lógica para importar dados de um arquivo CSV
-
-	// Verifique se o arquivo existe
-	if filePath == "" {
-		return errors.New("caminho do arquivo inválido")
-	}
-
-	// Leia o conteúdo do arquivo
-	dadosCSV, err := LerArquivoCSV(filePath)
-	if err != nil {
-		return errors.Wrap(err, "falha ao ler arquivo CSV")
-	}
-
-	// Faça o processamento necessário dos dados
-
-	// Salve os dados importados no banco de dados
-	err = s.SalvarDadosImportadosNoBancoDeDados(dadosCSV)
-	if err != nil {
-		return errors.Wrap(err, "falha ao salvar dados importados no banco de dados")
-	}
-
-	// Exemplo de impressão de mensagem de sucesso
-	fmt.Println("Dados importados com sucesso!")
-
-	return nil
-}
-
-func (s *ScrapLattes) ExportarDadosCSV(filePath string) error {
-	// Lógica para exportar dados para um arquivo CSV
-
-	// Verifique se há dados para exportar
-
-	// Obtenha os dados a serem exportados do banco de dados
-	dadosExportados, err := s.ObterDadosExportadosDoBancoDeDados()
-	if err != nil {
-		return errors.Wrap(err, "falha ao obter dados exportados do banco de dados")
-	}
-
-	// Faça o processamento necessário dos dados
-
-	// Escreva os dados no arquivo
-	err = EscreverDadosNoArquivoCSV(filePath, dadosExportados)
-	if err != nil {
-		return errors.Wrap(err, "falha ao escrever dados no arquivo CSV")
-	}
-
-	// Exemplo de impressão de mensagem de sucesso
-	fmt.Println("Dados exportados com sucesso!")
-
-	return nil
-}
-
-func LerArquivoCSV(filePath string) ([]string, error) {
-	// Lógica para ler um arquivo CSV e retornar os dados
-
-	// Implemente a lógica real para ler o arquivo CSV e extrair os dados
-	// Aqui está um exemplo simples que retorna dados fictícios:
-	dados := []string{"linha 1", "linha 2", "linha 3"}
-
-	return dados, nil
-}
-
-func (s *ScrapLattes) ObterPesquisadorPorIDLattes(IDLattes string) (*Pesquisador, error) {
-	// Lógica para obter um pesquisador do banco de dados pelo IDLattes
-
-	// Implemente a lógica real para obter o pesquisador pelo IDLattes
-	// Aqui está um exemplo simples que retorna um pesquisador fictício:
-	pesquisador := &Pesquisador{
-		Nome:     "Fulano de Tal",
-		Titulo:   "Doutor",
-		IDLattes: IDLattes,
-		Formacao: []string{"Bacharelado", "Mestrado", "Doutorado"},
-		Publicacoes: []Publicacao{
-			{Titulo: "Publicação 1", Ano: "2022"},
-			{Titulo: "Publicação 2", Ano: "2023"},
-		},
-	}
-
-	return pesquisador, nil
-}
-
-func (s *ScrapLattes) SalvarPesquisadorNoBancoDeDados(pesquisador *Pesquisador) error {
-	// Lógica para salvar um pesquisador no banco de dados
-
-	// Implemente a lógica real para salvar o pesquisador no banco de dados
-
-	return nil
-}
-
-func (s *ScrapLattes) AtualizarPesquisadorNoRepositorio(pesquisador *Pesquisador) error {
-	// Lógica para atualizar um pesquisador no repositório
-
-	// Implemente a lógica real para atualizar o pesquisador no repositório
-
-	return nil
-}
-
-func (s *ScrapLattes) VerificarExistenciaPesquisadorNoBancoDeDados(IDLattes string) (bool, error) {
-	// Lógica para verificar se um pesquisador existe no banco de dados
-
-	// Implemente a lógica real para verificar a existência do pesquisador no banco de dados
-
-	return true, nil
-}
-
-func (s *ScrapLattes) ObterPublicacoesDoPesquisador(pesquisador *Pesquisador) ([]Publicacao, error) {
-	// Lógica para obter as publicações de um pesquisador
-
-	// Implemente a lógica real para obter as publicações do pesquisador
-
-	return pesquisador.Publicacoes, nil
-}
-
-func (s *ScrapLattes) SalvarDadosImportadosNoBancoDeDados(dados []string) error {
-	// Lógica para salvar os dados importados no banco de dados
-
-	// Implemente a lógica real para salvar os dados importados no banco de dados
-
-	return nil
-}
-
-func EscreverDadosNoArquivoCSV(filePath string, dados []string) error {
-	// Lógica para escrever dados em um arquivo CSV
-
-	// Implemente a lógica real para escrever os dados no arquivo CSV
-
-	return nil
-}
-
-func (s *ScrapLattes) ObterDadosExportadosDoBancoDeDados() ([]string, error) {
-	// Lógica para obter os dados exportados do banco de dados
-
-	// Implemente a lógica real para obter os dados exportados do banco de dados
-	// Aqui está um exemplo simples que retorna dados fictícios:
-	dados := []string{"linha 1", "linha 2", "linha 3"}
-
-	return dados, nil
 }
