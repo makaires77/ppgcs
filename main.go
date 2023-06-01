4e0a92a86aa068b74af4a08413a334cd85cf7024
makaires77 <marcosaires1@gmail.com>
2023-06-01 12:12:12 -0300

    fuzzysearch

--- main.go
+++ main.go
@@ -14,7 +14,29 @@ import (
 
 	"github.com/makaires77/ppgcs/pkg/infrastructure/neo4j"
 	"github.com/makaires77/ppgcs/pkg/interfaces/rabbitmq"
+	"github.com/makaires77/ppgcs/ppgcs/pkg/fuzzysearch"
 )
+
+// idLattes, nome, tipo, titulo_do_capitulo, idioma, titulo_do_livro, ano, doi, pais_de_publicacao, isbn, nome_da_editora, numero_da_edicao_revisao, organizadores, paginas, autores,autores-endogeno, autores-endogeno-nome, tags, Hash,tipo_producao, natureza, titulo, nome_do_evento,ano_do_trabalho,pais_do_evento,cidade_do_evento,classificacao,periodico,volume,issn,estrato_qualis,editora,numero_de_paginas,numero_de_volumes
+
+func fuzz() {
+	fuzzyService := fuzzysearch.NewFuzzySearchService()
+
+	// Load data from CSV files
+	discentes, _ := fuzzyService.LoadCSVData("_data/powerbi/lista_docentes_colaboradores.csv", 2)
+	docente, _ := fuzzyService.LoadCSVData("_data/powerbi/publicacoes.csv", 1)
+	autores, _ := fuzzyService.LoadCSVData("_data/powerbi/publicacoes.csv", 14)
+
+	// Conduct fuzzy search for each docente
+	for _, discente := range discentes {
+		res, err := fuzzyService.FuzzySearchSetThreshold(discente, autores, 3)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("Result for '%s' with '0.82' threshold: %s\n", discente, strings.Join(res, " "))
+		}
+	}
+}
 
 // Estrutura de dados para representar um pesquisador
 type Researcher struct {
