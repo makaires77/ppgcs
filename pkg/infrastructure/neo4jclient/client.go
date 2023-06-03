80054aa9c6f470cccce8ba91905c513bf8c80ee3
makaires77 <marcosaires1@gmail.com>
2023-06-03 20:55:07 -0300

    todos erros corrijidos

--- pkg/infrastructure/neo4jclient/client.go
+++ pkg/infrastructure/neo4jclient/client.go
@@ -36,41 +36,34 @@ func (c *Neo4jClient) SavePublication(ctx context.Context, p publication.Publica
 	}
 	defer session.Close(context.Background())
 
-	_, err := session.WriteTransaction(ctx, func(tx neo4j.Transaction) (interface{}, error) {
-		result, err := tx.Run(
-			`CREATE (p:Publication {Natureza: $natureza, Titulo: $titulo, Idioma: $idioma, Periodico: $periodico, Ano: $ano, Volume: $volume, ISSN: $issn, EstratoQualis: $estrato_qualis, PaisDePublicacao: $pais_de_publicacao, Paginas: $paginas, DOI: $doi, Autores: $autores, AutoresEndogeno: $autores_endogeno, AutoresEndogenoNome: $autores_endogeno_nome, Tags: $tags, Hash: $hash})`,
-			map[string]interface{}{
-				"natureza":              p.Natureza,
-				"titulo":                p.Titulo,
-				"idioma":                p.Idioma,
-				"periodico":             p.Periodico,
-				"ano":                   p.Ano,
-				"volume":                p.Volume,
-				"issn":                  p.ISSN,
-				"estrato_qualis":        p.EstratoQualis,
-				"pais_de_publicacao":    p.PaisDePublicacao,
-				"paginas":               p.Paginas,
-				"doi":                   p.DOI,
-				"autores":               p.Autores,
-				"autores_endogeno":      p.AutoresEndogeno,
-				"autores_endogeno_nome": p.AutoresEndogenoNome,
-				"tags":                  p.Tags,
-				"hash":                  p.Hash,
-			},
-		)
-		if err != nil {
-			return nil, fmt.Errorf("neo4j: could not run transaction: %w", err)
-		}
-
-		if result.Err() != nil {
-			return nil, fmt.Errorf("neo4j: result error: %w", result.Err())
-		}
-
-		return nil, nil
-	})
-
+	result, err := session.Run(
+		ctx,
+		`CREATE (p:Publication {Natureza: $natureza, Titulo: $titulo, Idioma: $idioma, Periodico: $periodico, Ano: $ano, Volume: $volume, ISSN: $issn, EstratoQualis: $estrato_qualis, PaisDePublicacao: $pais_de_publicacao, Paginas: $paginas, DOI: $doi, Autores: $autores, AutoresEndogeno: $autores_endogeno, AutoresEndogenoNome: $autores_endogeno_nome, Tags: $tags, Hash: $hash})`,
+		map[string]interface{}{
+			"natureza":              p.Natureza,
+			"titulo":                p.Titulo,
+			"idioma":                p.Idioma,
+			"periodico":             p.Periodico,
+			"ano":                   p.Ano,
+			"volume":                p.Volume,
+			"issn":                  p.ISSN,
+			"estrato_qualis":        p.EstratoQualis,
+			"pais_de_publicacao":    p.PaisDePublicacao,
+			"paginas":               p.Paginas,
+			"doi":                   p.DOI,
+			"autores":               p.Autores,
+			"autores_endogeno":      p.AutoresEndogeno,
+			"autores_endogeno_nome": p.AutoresEndogenoNome,
+			"tags":                  p.Tags,
+			"hash":                  p.Hash,
+		},
+	)
 	if err != nil {
-		return fmt.Errorf("neo4j: could not execute write transaction: %w", err)
+		return fmt.Errorf("neo4j: could not execute query: %w", err)
+	}
+
+	if result.Err() != nil {
+		return fmt.Errorf("neo4j: query result error: %w", result.Err())
 	}
 
 	return nil
