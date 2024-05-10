39e7d7c099e26e70c03b512b0935d722e0724352
makaires77 <marcos@bahlis.com.br>
2024-05-10 00:05:39 -0300

    extração dados linhas pesquisa Fiocruz Ceará

--- source/domain/scraper_fiocruz_ceara.py
+++ source/domain/scraper_fiocruz_ceara.py
@@ -33,13 +33,13 @@ class FiocruzCearaScraper:
             return None
 
     def scrape(self):
-        driver = webdriver.Chrome()  # você precisa configurar o WebDriver para o navegador que você está usando
-        driver.get(self.url)
+        driver = webdriver.Chrome()
+        driver.get(self.base_url)
 
         thematic_areas = []
 
         try:
-            # Espera até que os elementos de interesse estejam disponíveis na página
+            # Esperar até que os elementos de interesse estejam disponíveis na página
             thematic_area_elements = WebDriverWait(driver, 10).until(
                 EC.presence_of_all_elements_located((By.CLASS_NAME, "thematic-area"))
             )
@@ -47,11 +47,11 @@ class FiocruzCearaScraper:
             for thematic_area_element in thematic_area_elements:
                 area_name = thematic_area_element.find_element(By.TAG_NAME, 'h5').text.strip()
 
-                # Encontra o botão dentro do elemento de área temática e clica nele
+                # Encontrar o botão dentro do elemento de área temática e clica nele
                 button = thematic_area_element.find_element(By.TAG_NAME, 'button')
                 button.click()
 
-                # Espera até que o conteúdo colapsável esteja visível
+                # Esperar até que o conteúdo colapsável esteja visível
                 collapse_content = WebDriverWait(driver, 10).until(
                     EC.visibility_of_element_located((By.CSS_SELECTOR, button.get_attribute('data-target')))
                 )
@@ -65,11 +65,10 @@ class FiocruzCearaScraper:
                     'link': link
                 })
 
-                # Fecha o conteúdo colapsado
                 button.click()
                 
         finally:
-            driver.quit()  # Garante que o navegador seja fechado mesmo em caso de exceção
+            driver.quit()
 
         return json.dumps(thematic_areas, indent=4)
 
@@ -86,30 +85,46 @@ class FiocruzCearaScraper:
             return []
 
         areas = []
-
-        content_section = page_content.find('section', class_='fiocruz-areas')
-        conteiner_section = content_section.find('section', class_='container')
-        if content_section:
-            
-            area_section = content_section.find('div', class_='container')
-            if area_section:
-                articles = area_section.find_all('article', class_='team-cards')
-                for article in articles:
-                    area_name = article.find('div', class_='team-name').get_text(strip=True) if article.find('div', class_='team-name') else ''
-                    team_count = article.find('div', class_='dept-name').get_text(strip=True) if article.find('div', class_='dept-name') else ''
-                    image_url = article.find('img')['src'] if article.find('img') else ''
-
-                    area_url = article.find('a')['href'] if article.find('a') else ''
-                    area_details = self.extract_research_area_details(area_url)
-
-                    area_info = {
-                        'area_name': area_name,
-                        'team_count': team_count,
-                        'image_url': image_url,
-                        'details': area_details
-                    }
-                    areas.append(area_info)
-
+        pesq_line = []
+        pesq_texts=''
+        area_divs = page_content.find_all('div', class_='fiocruz-card')
+        if area_divs:
+            for area in area_divs:
+                pesquisadores=[]
+                area_name = area.find('h5').get_text(strip=True) if area.find('h5') else ''
+                area_desc = area.find('p').get_text(strip=True).replace('\n',' ') if area.find('p') else ''
+                area_desc = area_desc.replace('                                             ',' ')
+                area_url = area.find('a')['href'] if area.find('a') else ''
+                area_content = self.get_html(area_url)
+                area_plats = area_content.find_all('p')
+                area_plats = [x.get_text().replace('\xa0',' ') for x in area_plats]
+                if 'digital' in area_url:
+                    pesq_url = os.path.join(area_url+'/pesquisadores-em-saude-digital/')
+                else:
+                    pesq_url = os.path.join(area_url+'/pesquisadores/')
+                pesq_content = self.get_html(pesq_url)
+                if 'familia' in area_url or 'ambiente' in area_url:
+                    pesq_names = pesq_content.find_all('h5')
+                else:
+                    pesq_names = pesq_content.find_all('p', class_='has-black-color has-text-color has-medium-font-size')
+                pesq_names = [x.get_text().strip() for x in pesq_names]
+                for i in pesq_names:
+                    if 'Dr' not in i and 'Família' not in i and 'Ambiente' not in i:
+                        pesquisadores.append(i.replace('  ',' '))
+                pesq_dados = pesq_content.find_all('p', class_='has-small-font-size')
+                pesq_texts = [x.get_text() for x in pesq_dados]
+                for i in pesq_texts:
+                    if '– ' in i[:4]:
+                        pesq_line.append(i.strip())
+                area_info = {
+                    'area_name': area_name,
+                    'area_desc': area_desc,
+                    # 'area_url': area_url,
+                    'area_plats': area_plats,
+                    'area_names': pesquisadores,
+                    'area_lines': pesq_line
+                }
+                areas.append(area_info)
         return areas
 
     def scrape_main_page_quantitative_data(self):
@@ -167,12 +182,12 @@ class FiocruzCearaScraper:
 
         details = {}
 
-        # Extracting the main title of the research area
+        # Extrair titulo principal da área de pesquisa
         title_div = page_content.find('div', id='header-infos')
         if title_div:
             details['title'] = title_div.find('h1').get_text(strip=True) if title_div.find('h1') else ''
 
-        # Extracting research directors
+        # Extrair diretores
         directors = []
         for member in page_content.find_all('div', class_='member'):
             name = member.find('div', class_='member-name').get_text(strip=True) if member.find('div', class_='member-name') else ''
